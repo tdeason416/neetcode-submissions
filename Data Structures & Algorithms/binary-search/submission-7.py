dbb835fd217fe726aa7aee2cb66f4d01767cc462
Travis Deason <tdeason416@gmail.com>
2026-08-05 12:21:11 -0700

    Add: binary-search - submission-7

---
+++ Data Structures & Algorithms/binary-search/submission-7.py
@@ -0,0 +1,31 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+
+        idx = int(len(nums) / 2 - 0.5)
+        idx_global = idx
+        val = nums[idx]
+        print(idx_global, val)
+
+        while val != target:
+            if target > val:
+                nums = nums[idx+1:]
+                idx = int(len(nums) / 2 - 0.5)
+                idx_global = idx_global + idx + 1
+                print(f'upround nums = {nums}, gidx = {idx_global} lidx={idx} val={val}')
+            elif target < val:
+                nums = nums[:idx]
+                idx = int(len(nums) / 2 - 0.5)
+                idx_global = idx_global - len(nums) + idx
+                print(f'downround nums = {nums} gidx = {idx_global} lidx={idx} val={val}')
+            if len(nums) == 0:
+                return -1
+            newval = nums[idx]
+            # if val == newval:
+                # return -1
+            val = newval
+
+        return idx_global
+
+
+
+        
