e13be93ebd1bb19087fbf3e54c0c01414bf7d5ff
Travis Deason <tdeason416@gmail.com>
2026-08-28 15:23:15 -0700

    Add: sliding-window-maximum - submission-13

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-13.py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self, idx, val):
+        self.idx = idx
+        self.val = val
+        self.morethan = None
+
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        maxNode = Node(0, nums[0])
+        output = []
+        if k == 1:
+            output.append(maxNode.val)
+        for idx in range(1, len(nums)):
+            newNode = Node(idx, nums[idx])
+            if newNode.val > maxNode.val:
+                newNode.morethan = maxNode
+                maxNode = newNode
+            else:
+                compNode = maxNode
+                # while True:
+                for _ in range(5):
+                    nextComp = compNode.morethan
+                    if not nextComp:
+                        compNode.morethan = newNode
+                        break
+                    elif newNode.val >= nextComp.val:
+                        newNode.morethan = nextComp
+                        compNode.morethan = newNode
+                        break
+                    compNode = compNode.morethan
+            if idx >= k - 1:
+                while maxNode.idx <= idx - k:
+                    maxNode = maxNode.morethan
+            if idx >= k-1:
+                output.append(maxNode.val)
+        return output
+        
+
+
+
+        
