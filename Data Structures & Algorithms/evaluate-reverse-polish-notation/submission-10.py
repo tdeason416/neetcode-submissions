614161ad889ffbc5dca6ec3b2b39ab40dea15f82
Travis Deason <tdeason416@gmail.com>
2026-08-30 11:16:02 -0700

    Add: evaluate-reverse-polish-notation - submission-10

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-10.py
@@ -0,0 +1,48 @@
+class Arithmitic:
+    def __init__(self):
+        self.nums = []
+        # self.armap = {"+": self.add, '-': self.sub, '*': self.mult, '/': self.div}
+        self.armap = {
+            "+": lambda a, b: a + b,
+            "-": lambda a, b: a - b,
+            "*": lambda a, b: a * b,
+            "/": lambda a, b: int(a / b)
+        }
+    
+    def operation(self, tok):
+        if tok in self.armap:
+            n2 = self.nums.pop()
+            n1 = self.nums.pop()
+            self.nums.append(self.armap[tok](n1,n2))
+        # if tok == '+':
+        #     # self.add()
+        #     n2 = self.nums.pop()
+        #     n1 = self.nums.pop()
+        #     self.nums.append(n1 + n2)
+        # elif tok == '-':
+        #     # self.sub()
+        #     n2 = self.nums.pop()
+        #     n1 = self.nums.pop()
+        #     self.nums.append(n1 - n2)
+        # elif tok == '*':
+        #     # self.mult()
+        #     n2 = self.nums.pop()
+        #     n1 = self.nums.pop()
+        #     self.nums.append(n1 * n2)
+        # elif tok == '/':
+        #     # self.div()
+        #     n2 = self.nums.pop()
+        #     n1 = self.nums.pop()
+        #     self.nums.append(int(n1 / n2))
+        else:
+            self.nums.append(int(tok))
+
+    
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        m = Arithmitic()
+        for n in tokens:
+            m.operation(n)
+        return m.nums[-1]
+        
+        
